bc0acee7133b54a68590376259696bf8e100cba7
Muqadas Ejaz <muqadasejaz3@gmail.com>
2025-09-10 09:55:36 +0500

    Add files via upload

---
+++ 01_hello world.py
@@ -0,0 +1,6 @@
+
+# write a program that prints "hello world!" with your name
+
+name = input("Enter your name:")
+
+print("hello world, "+ name)
---
+++ 02_even or odd.py
@@ -0,0 +1,8 @@
+# Write a program to check whether a no is even or odd
+
+num=int(input("enter the num: "))
+
+if num % 2 == 0:
+    print(num, " is even number")
+else:
+    print(num, " is odd number")
