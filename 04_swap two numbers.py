168d6a3d1da22a7f81a7111ab4df3bfd2423ad0c
Muqadas Ejaz <muqadasejaz3@gmail.com>
2025-09-11 10:20:36 +0500

    Add files via upload

---
+++ 04_swap two numbers.py
@@ -0,0 +1,12 @@
+# Write a program to swap values of two numbers
+
+a=int(input("enter the number of a: "))
+b=int(input("enter the number of b: "))
+
+print("the number of a before swaping is", a , "the number of b before swapping is", b)
+
+c=a
+a=b
+b=c
+
+print("the number of a after swaping is", a , "the number of b after swapping is", b)
