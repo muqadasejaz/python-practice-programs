1c75895dbab2b17f8993258951e59955b2acdf68
Muqadas Ejaz <muqadasejaz3@gmail.com>
2025-09-14 15:12:17 +0500

    Add files via upload

---
+++ 07_area_of_circle.py
@@ -0,0 +1,8 @@
+#write a program to calculate area of circle
+
+pi=3.14
+radius=int(input("Enter the radius of a circle:"))
+
+area =pi *radius*radius
+
+print(f"The area of circle with radius {radius} is {area:.2f}")
---
+++ 08_perimeter_of_rectangle.py
@@ -0,0 +1,7 @@
+#write a program to calculate perimeter of rectangle
+
+length=int(input("Enter the length of rectangle:"))
+width=int(input("Enter the width of rectangle:"))
+
+perimeter=2*(length+width)
+print(f"The perimeter of rectangle with length {length} and width {width} is {perimeter}")
